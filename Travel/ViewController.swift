93462205f1ec956d16d5da74c8fe0bc2564f2ab0
Juan Morillo <juanmorillios@gmail.com>
2016-07-20 15:56:33 +0200

    App Travel
    Reto 1 Sección 5, Clase 46 Curso iOS 10 y Swift 3

--- Travel/ViewController.swift
+++ Travel/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var helloViajero: UILabel!
+    @IBOutlet weak var namePassenger: UITextField!
+    @IBOutlet weak var nameDestino: UITextField!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+      
+        helloViajero.text = ""
+    
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buttonViajarPressed(_ sender: UIButton) {
+        
+        let alertController = UIAlertController(title: "🛫Bienvenido:\(namePassenger.text!)?", message: "Ha elegido el siguiente destino 🛬: \(nameDestino.text!)", preferredStyle: .alert)
+    
+       present(alertController, animated: true, completion: nil)
+        
+       let okAction = UIAlertAction(title: "Confirmar ✈️", style: .default, handler: nil)
+        
+        let cancelAction = UIAlertAction(title: "Cancelar ", style: .default, handler: nil)
+        
+        helloViajero.text = "Nombre Pasajero:: \(namePassenger.text!) destino seleccionado: \(nameDestino.text!) Felíz Viaje!!!"
+        
+        
+        
+        alertController.addAction(okAction)
+        alertController.addAction(cancelAction)
+    }
 
 }
 
